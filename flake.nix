64aa2241226190d35b679511378c558d346a8024
kyu <rouven-pascal.bender@gmx.de>
2025-05-27 22:38:36 +0200

    made the root path work

--- flake.nix
+++ flake.nix
@@ -5,24 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-      devShells.${system}.default  =
-      pkgs.mkShell
-        {
-        	buildInputs = [
-        		pkgs.go
+	outputs = { self, nixpkgs }:
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in
+	{
+		devShells.${system}.default  = pkgs.mkShell
+		{
+			buildInputs = [
+				pkgs.go
 				pkgs.yarn
-				pkgs.esbuild
 			];
 
-          shellHook = ''
-            echo "Hello World"
-          '';
-        };
-  };
+			shellHook = ''
+				echo "Hello World"
+			'';
+		};
+	};
 }
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	_ "embed"
 	"log"
 	"net/http"
 )
 
+//go:embed static/entry.html
+var indexhtml []byte
+
 func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./static"))
-	//mux.HandleFunc("GET /", homepage)
+	mux.HandleFunc("GET /", homepage)
 
 	mux.Handle("GET /cdn/{filename}", http.StripPrefix("/cdn/", fs))
 
@@ -16,4 +20,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write(indexhtml)
 }
